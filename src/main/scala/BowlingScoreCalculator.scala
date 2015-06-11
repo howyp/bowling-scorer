9e841e57486f2af2afbebf1ada82cdfe38ccbcd9
Howy Perrin <howard.perrin@digital.hmrc.gov.uk>
2015-06-11 17:25:55 +0200

    Reformat the calculator

--- src/main/scala/BowlingScoreCalculator.scala
+++ src/main/scala/BowlingScoreCalculator.scala
@@ -1,17 +1,16 @@
 trait BowlingScoreCalculator {
   def score(frames: List[Frame]): Int = frames match {
-    case Regular(b1, b2) :: remain  => b1 + b2 + score(remain)
-    case Spare(_)        :: remain  => 10 + balls(remain).head + score(remain)
-    case Strike          :: remain  => 10 + balls(remain).sum  + score(remain)
-    case Nil |
-         Bonus(_, _)     :: _     => 0
+    case Regular(b1, b2)   :: remain  => b1 + b2 + score(remain)
+    case Spare(_)          :: remain  => 10 + twoBalls(remain).head + score(remain)
+    case Strike            :: remain  => 10 + twoBalls(remain).sum  + score(remain)
+    case Nil | Bonus(_, _) :: _       => 0
   }
 
-  def balls(frames: List[Frame]): Stream[Int] = frames match {
-    case Regular(b1, b2) :: remain => b1 #:: b2                 #:: Stream.Empty
-    case Bonus(b1, b2)   :: remain => b1 #:: b2                 #:: Stream.Empty
-    case Spare(b1)       :: remain => b1 #:: (10 - b1)          #:: Stream.Empty
-    case Strike          :: remain => 10 #:: balls(remain).head #:: Stream.Empty
+  def twoBalls(frames: List[Frame]): Stream[Int] = frames match {
+    case Regular(b1, b2) :: remain => b1 #:: b2                    #:: Stream.Empty
+    case Bonus(b1, b2)   :: remain => b1 #:: b2                    #:: Stream.Empty
+    case Spare(b1)       :: remain => b1 #:: (10 - b1)             #:: Stream.Empty
+    case Strike          :: remain => 10 #:: twoBalls(remain).head #:: Stream.Empty
     case Nil                       => Stream.Empty
   }
 }
