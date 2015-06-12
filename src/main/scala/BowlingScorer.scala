740a0f8d5bfad2f112732f4ea52f4ad1df7c0484
Howy Perrin <howard.perrin@digital.hmrc.gov.uk>
2015-06-12 15:07:39 +0200

    Move logic

--- src/main/scala/BowlingScorer.scala
+++ src/main/scala/BowlingScorer.scala
@@ -1,5 +1,14 @@
 import scala.util.parsing.combinator.RegexParsers
 
 object BowlingScorer extends RegexParsers with BowlingScoreCalculator {
-  def parseGame(g: String): Int = score(BowlingScoreParser().parse(g))
+  def parseGame(g: String): Int = {
+    val parse = BowlingScoreParser().parse(g)
+    val s = score(parse)
+    println(
+      s"""Parsed game $g
+         |         as $parse
+         |      balls ${balls(parse).force}
+         |      score $s""".stripMargin)
+    s
+  }
 }
--- src/main/scala/Frame.scala
+++ src/main/scala/Frame.scala
@@ -1,5 +1,5 @@
-sealed trait Frame
-case object Strike extends Frame
-case class Spare(ball1: Int) extends Frame
-case class Regular(ball1: Int, ball2: Int) extends Frame
-case class Bonus(ball1: Int, ball2: Int) extends Frame
+sealed trait Frame { val balls: Stream[Int] }
+case class Regular(ball1: Int, ball2: Int) extends Frame { val balls = Stream(ball1, ball2) }
+case class Bonus(ball1: Int, ball2: Int)   extends Frame { val balls = Stream(ball1, ball2) }
+case class Spare(ball1: Int)               extends Frame { val balls = Stream(ball1, 10 - ball1) }
+case object Strike                         extends Frame { val balls = Stream(10) }
